53b2f9804637dd85db1342d8725933837f7a6596
Saurav <sauravkumar00108@gmail.com>
2024-12-20 16:52:38 +0530

    Add axios dependency and update API URL; implement fetching of master categories in ServiceDetails component

--- src/Service/api-path.js
+++ src/Service/api-path.js
@@ -1,15 +1,5 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'http://localhost:5656';
 
-export const registerCompany = (data) => axios.post(`${API_URL}/auth/signup`, data);
-export const verifyEmailOtp = (data) => axios.post(`${API_URL}/otp/verify-email`, data);
-export const verifyMobileOtp = (data) => axios.post(`${API_URL}/otp/verify-phone`, data);
-export const createJob = (data,token) => axios.post(`${API_URL}/jobs`, data,{
-  headers: { Authorization: token },
-});
-
-export const postJob = (data, token) =>
-  axios.post(`${API_URL}/jobs`, data, {
-    headers: { Authorization: token },
-  });
+export const getMastercategory = () => axios.get(`${API_URL}/api/get/mastercat`);
--- src/components/Home/ServiceDetails.js
+++ src/components/Home/ServiceDetails.js
@@ -1,23 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import { CiStar } from "react-icons/ci";
 import { PiUsersThreeLight } from "react-icons/pi";
+import { getMastercategory } from '../../Service/api-path';
 function ServiceDetails() {
+    const [services, setServices] = React.useState([])
 
-    const services = [
-        { name: "Women's Salon & Spa", icon: "👩‍🦰" },
-        { name: "Men's Salon & Massage", icon: "🧔‍♂️" },
-        { name: "AC & Appliance Repair", icon: "❄️" },
-        { name: "Cleaning", icon: "🧹" },
-        { name: "Electrician, Plumber & Carpenter", icon: "🔧" },
-        { name: "Electrician, Plumber & Carpenter", icon: "🔧" },
-        { name: "Native Water Purifier", icon: "💧" },
-        { name: "Men's Salon & Massage", icon: "🧔‍♂️" },
-        { name: "AC & Appliance Repair", icon: "❄️" },
-        { name: "Men's Salon & Massage", icon: "🧔‍♂️" },
-        { name: "AC & Appliance Repair", icon: "❄️" },
-    ];
+    const fetchMasterCategory = async () => {
+        try {
+            const data = await getMastercategory();
+            console.log('data: ', data);
+            const filteredData = data.data.map((item) => {
+                return {
+                    name: item.masterName,
+                    icon: item.image_url,
+                    masterId: item.masterId
+                };
+            });
+            console.log('filteredData: ', filteredData);
+
+            setServices(filteredData);
+            // console.log('Master Category Data:', data);
+        } catch (error) {
+            console.error('Error in fetching master category:', error);
+        }
+    };
+    useEffect(() => {
+
+        fetchMasterCategory();
+    }, []);
+
+    // const services = [
+    //     { name: "Women's Salon & Spa", icon: "👩‍🦰" },
+    //     { name: "Men's Salon & Massage", icon: "🧔‍♂️" },
+    //     { name: "AC & Appliance Repair", icon: "❄️" },
+    //     { name: "Cleaning", icon: "🧹" },
+    //     { name: "Electrician, Plumber & Carpenter", icon: "🔧" },
+    //     { name: "Electrician, Plumber & Carpenter", icon: "🔧" },
+    //     { name: "Native Water Purifier", icon: "💧" },
+    //     { name: "Men's Salon & Massage", icon: "🧔‍♂️" },
+    //     { name: "AC & Appliance Repair", icon: "❄️" },
+    //     { name: "Men's Salon & Massage", icon: "🧔‍♂️" },
+    //     { name: "AC & Appliance Repair", icon: "❄️" },
+    // ];
 
     const chunkArray = (array, chunkSize) => {
         const result = [];
@@ -37,17 +63,30 @@ function ServiceDetails() {
             </h1>
 
             <div className='md:flex gap-10'>
-                <div className="md:w-[70%] ">
+                <div className="md:w-[70%] w-full mx-auto px-4">
+                    {/* Carousel Section */}
                     <Carousel autoPlay interval={3000} infiniteLoop showArrows={false}>
                         {serviceChunks?.map((chunk, chunkIndex) => (
-                            <div key={chunkIndex} className="grid grid-cols-2 md:grid-cols-3 gap-4 mb-8 mt-[7%]">
+                            <div
+                                key={chunkIndex}
+                                className="grid grid-cols-2 md:grid-cols-3 gap-6 mb-8 mt-6"
+                            >
                                 {chunk?.map((service, index) => (
                                     <div
                                         key={index}
-                                        className="flex flex-col  items-center justify-between p-4 bg-white shadow-md rounded-lg hover:shadow-lg transition h-40"
+                                        className="relative flex flex-col items-center justify-end p-3 bg-white shadow-md rounded-xl hover:shadow-lg transition transform hover:scale-105 h-36 overflow-hidden"
                                     >
-                                        <div className="text-3xl">{service.icon}</div>
-                                        <p className="mt-2 text-center text-xs md:text-base font-medium font-montserrat">
+                                        <div className="relative w-full h-36">
+                                            {/* Image with Shadow */}
+                                            <img
+                                                className="h-full w-full object-cover rounded-t-lg shadow-lg"
+                                                src={service.icon}
+                                                alt={service.name}
+                                            />
+                                            <div className="absolute inset-0 bg-gradient-to-t  from-black/80 via-transparent to-transparent rounded-t-lg"></div>
+                                        </div>
+                                        {/* Text on Image */}
+                                        <p className="absolute bottom-0 w-full text-center flex justify-center items-center md:mt-0 text-[11px] md:text-[14px] md:text-base font-normal font-montserrat text-white bg-black/90 h-12">
                                             {service.name}
                                         </p>
                                     </div>
@@ -55,23 +94,30 @@ function ServiceDetails() {
                             </div>
                         ))}
                     </Carousel>
-                    <div className='flex gap-12 mt-[-5%]'>
-                        <div className=' flex'>
-                            <CiStar size={30} className='' />
+
+                    {/* Ratings Section */}
+                    <div className="flex flex-col md:flex-row gap-8 items-center justify-center mt-4">
+                        <div className="flex items-center gap-4">
+                            <CiStar size={30} className="text-yellow-500" />
                             <div>
-                                <p className='pl-8 text-xl font-bold flex flex-col font-montserrat'>4.8</p>
-                                <p className='pl-8 text-[#321c6d] text-sm font-bold font-urbanist'>Service rating</p>
+                                <p className="text-2xl font-bold font-montserrat">4.8</p>
+                                <p className="text-sm font-bold text-[#321c6d] font-urbanist">
+                                    Service Rating
+                                </p>
                             </div>
                         </div>
-                        <div className=' flex'>
-                            <PiUsersThreeLight size={30} />
+                        <div className="flex items-center gap-4">
+                            <PiUsersThreeLight size={30} className="text-blue-500" />
                             <div>
-                                <p className='pl-8  text-xl font-bold flex flex-col  font-montserrat'>12M+</p>
-                                <p className='pl-8 text-[#321c6d] text-sm font-bold font-urbanist'>Customers Globally</p>
+                                <p className="text-2xl font-bold font-montserrat">12M+</p>
+                                <p className="text-sm font-bold text-[#321c6d] font-urbanist">
+                                    Customers Globally
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
+
                 <div className="md:flex hidden w-[30%] md:w-[70%] ">
                     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-1 max-[450px]:grid-cols-1  max-[600px]:grid-cols-2 min-[600px]:grid-cols-2 '>
                         {/* Top Image */}
